3ace624dee26f69afa7da2604c0a96325a723f78
s2yeji <s2seolyeji@gmail.com>
2024-06-11 18:11:46 +0900

    20240611

--- src/App.js
+++ src/App.js
@@ -4,8 +4,10 @@ import './styles/style.css';
 import Header from './layouts/Header';
 import Footer from './layouts/Footer';
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
+import Webzine from './pages/webzine/Webzine';
+
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
@@ -14,6 +16,7 @@ function App() {
         <Header />
         <Routes>
           <Route path="/" element={<Main />} />
+          <Route path="/webzine" element={<Webzine />} />
         </Routes>
         <Footer />
       </div>
