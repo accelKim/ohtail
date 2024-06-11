f7e8850a8e526ceea3f870dcf8d8b8e6800af334
accelkim <dnjsgh567877@gmail.com>
2024-06-11 09:19:16 +0900

    layouts

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import './styles/reset.css';
 import './styles/style.css';
 
-import Header from './components/Header';
-import Footer from './components/Footer';
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
