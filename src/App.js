700bfcb72ac0a28aef6c79d4f4a4519e1c657cfa
accelkim <dnjsgh567877@gmail.com>
2024-06-11 17:48:59 +0900

    first recipe page

--- src/App.js
+++ src/App.js
@@ -6,19 +6,25 @@ import Footer from './layouts/Footer';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
+import Recipe from './pages/recipe/RecipeList';
+import RecipeDetail from './pages/recipe/RecipeDetail';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="wrap">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="wrap">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    
+                    {/* 레시피 페이지 */}
+                    <Route path="/recipe" element={<Recipe />} />
+                    <Route path="/cocktail/:id" element={<RecipeDetail />} />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
