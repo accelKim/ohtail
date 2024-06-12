6220a8e9d05b362a9ed40786ddf07a7247a58344
accelkim <dnjsgh567877@gmail.com>
2024-06-12 10:12:07 +0900

    merged

--- src/App.js
+++ src/App.js
@@ -1,20 +1,14 @@
-import "./styles/reset.css";
-import "./styles/style.css";
+import './styles/reset.css';
+import './styles/style.css';
 
-import Header from "./layouts/Header";
-import Footer from "./layouts/Footer";
-
-
-
-
-
-
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Main from './pages/main/Main';
 import Recipe from './pages/recipe/RecipeList';
 import RecipeDetail from './pages/recipe/RecipeDetail';
-import CreateMyRecipe from "./pages/myRecipe/CreateMyRecipe";
+import CreateMyRecipe from './pages/myRecipe/CreateMyRecipe';
 import Webzine from './pages/webzine/Webzine';
 
 function App() {
@@ -24,13 +18,13 @@ function App() {
                 <Header />
                 <Routes>
                     <Route path="/" element={<Main />} />
-                    
                     {/* 레시피 페이지 */}
                     <Route path="/recipe" element={<Recipe />} />
                     <Route path="/recipe/:id" element={<RecipeDetail />} />
                     {/* 마이 레시피 */}
+                    <Route path="/createMyRecipe" element={<CreateMyRecipe />} />
+                    {/* 웹진 */}
                     <Route path="/webzine" element={<Webzine />} />
-          <Route path="/createMyRecipe" element={<CreateMyRecipe />} />
                 </Routes>
                 <Footer />
             </div>
