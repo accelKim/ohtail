6a7166a63ef6171c8b9eccdad9c7b6430021164f
accelkim <dnjsgh567877@gmail.com>
2024-06-15 09:36:40 +0900

    resoleved

--- src/App.js
+++ src/App.js
@@ -1,42 +1,40 @@
-import "./styles/reset.css";
+import './styles/reset.css';
 
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Header from "./layouts/Header";
-import Footer from "./layouts/Footer";
-import Main from "./pages/main/Main";
-import Recipe from "./pages/recipe/RecipeList";
-import Singup from "./pages/singup/Singup";
-import MyRecipeList from "./pages/myRecipe/MyRecipeList";
+import Header from './layouts/Header';
+import Footer from './layouts/Footer';
+import Main from './pages/main/Main';
+import MyRecipeList from './pages/myRecipe/MyRecipeList';
 import RecipeDetail from './pages/recipe/RecipeDetail';
 import CreateMyRecipe from './pages/myRecipe/CreateMyRecipe';
 import Webzine from './pages/webzine/Webzine';
 import Login from './pages/login/Login';
-import Singup from './pages/singup/Singup';
+import Signup from './pages/signup/Signup';
 import RecipeListPage from './pages/recipe/RecipeListPage';
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="wrap">
-        <Header />
-        <Routes>
-          <Route path="/" element={<Main />} />
-          {/* 레시피 페이지 */}
-          <Route path="/recipe" element={<RecipeListPage />} />
-          <Route path="/recipe/:id" element={<RecipeDetail />} />
-          {/* 마이 레시피 */}
-          <Route path="/myRecipe" element={<MyRecipeList />} />
-          <Route path="/createMyRecipe" element={<CreateMyRecipe />} />
-          {/* 웹진 */}
-          <Route path="/webzine" element={<Webzine />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-        </Routes>
-        <Footer />
-      </div>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <div className="wrap">
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Main />} />
+                    {/* 레시피 페이지 */}
+                    <Route path="/recipe" element={<RecipeListPage />} />
+                    <Route path="/recipe/:id" element={<RecipeDetail />} />
+                    {/* 마이 레시피 */}
+                    <Route path="/myRecipe" element={<MyRecipeList />} />
+                    <Route path="/createMyRecipe" element={<CreateMyRecipe />} />
+                    {/* 웹진 */}
+                    <Route path="/webzine" element={<Webzine />} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Signup />} />
+                </Routes>
+                <Footer />
+            </div>
+        </BrowserRouter>
+    );
 }
 
 export default App;
