24cd4fc72f6ee54f401c50b3d920885caa06a2d5
accelkim <dnjsgh567877@gmail.com>
2024-07-09 16:40:25 +0900

    1

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api',
+  baseURL: 'https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
