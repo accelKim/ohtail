dfbd819124e323987f32b27cfd7bb926945e131c
accelkim <dnjsgh567877@gmail.com>
2024-07-10 18:08:12 +0900

    test12

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -14,7 +14,7 @@ const Chatbot = () => {
         try {
             const userMessage = { role: 'user', content: message };
             setChatHistory((prevHistory) => [...prevHistory, userMessage]);
-            const response = await axios.post(`https://ohtail:80/api/chatbot`, {
+            const response = await axios.post(`https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api/chatbot`, {
                 userPrompt: message,
             });
             const botMessage = { role: 'bot', content: response.data };
