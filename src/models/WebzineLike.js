1d0a8dd3c9408b050b216829597a98181e839b57
accelkim <dnjsgh567877@gmail.com>
2024-07-10 17:04:31 +0900

    .

--- src/models/WebzineLike.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const webzineLikeSchema = new mongoose.Schema({
-  webzineId: { type: String, required: true },
-  userId: { type: String, required: true },
-});
-
-const WebzineLike = mongoose.model("WebzineLike", webzineLikeSchema);
-module.exports = WebzineLike;
