087dfa39393357144091bb833db291c342135789
accelkim <dnjsgh567877@gmail.com>
2024-07-10 18:22:03 +0900

    test13

--- src/components/chatbot/Chatbot.jsx
+++ src/components/chatbot/Chatbot.jsx
@@ -2,7 +2,6 @@ import axios from 'axios';
 import React, { useState } from 'react';
 import style from '../../styles/Chatbot.module.css';
 const apiUrl = process.env.REACT_APP_API_URL;
-
 const Chatbot = () => {
     const [message, setMessage] = useState('');
     const [chatHistory, setChatHistory] = useState([]);
@@ -14,7 +13,7 @@ const Chatbot = () => {
         try {
             const userMessage = { role: 'user', content: message };
             setChatHistory((prevHistory) => [...prevHistory, userMessage]);
-            const response = await axios.post(`https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api/chatbot`, {
+            const response = await axios.post(`${apiUrl}/api/chatbot`, {
                 userPrompt: message,
             });
             const botMessage = { role: 'bot', content: response.data };
