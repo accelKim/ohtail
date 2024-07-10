4cc7ce123575e6e12f4a8354043b8b2d895165aa
accelkim <dnjsgh567877@gmail.com>
2024-07-10 16:00:21 +0900

    test11

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
-
+const apiUrl = process.env.REACT_APP_API_URL || 'https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api';
 const api = axios.create({
-  baseURL: 'http://localhost:8080',
+  baseURL: apiUrl,
   headers: {
     'Content-Type': 'application/json',
   },
--- src/store/ref.js
+++ src/store/ref.js
@@ -1,1 +1,2 @@
-export const url = `http://localhost:8080`;
+const apiUrl = process.env.REACT_APP_API_URL || 'https://web-ohtail-ly8dqscw04c35e9c.sel5.cloudtype.app/api';
+export const url = `${apiUrl}`;
